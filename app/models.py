2ff7ea09e9a63ad829eeab047592e8e694afc693
charging-kuafuai <charging@126.com>
2024-01-17 11:16:21 +0800

    管理源动态的创建表单，表单内容包括但不限于员工卡号，姓名，电话，是否带家属，是否坐班车，是否参加等 普通用户通过管理员创建的表单填写信息并保存到数据库 管理员可以查看并导出普通用户填写的表单

--- app/models.py
+++ app/models.py
@@ -1,16 +1,1 @@
-class FormData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    employee_id = db.Column(db.String(20), nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=False)
-    has_family = db.Column(db.Boolean, nullable=False)
-    has_shuttle = db.Column(db.Boolean, nullable=False)
-    has_activity = db.Column(db.Boolean, nullable=False)
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @staticmethod
-    def get_all():
-        return FormData.query.all()
+在`app/models.py`文件中创建以下模型类：
